9c6d2de0a2d7dbec3078ebbb197844d6c1c357f5
Keishi <natsusome@qq.com>
2022-02-24 21:54:25 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,8 @@ const autometa_options = {
     canonical_base: 'https://hello.mckfs.com',
 };
 
+const moment = require('moment');
+
 module.exports = {
     title: 'KFS 服务器',
     description: 'KFS服务器全称“KizunaAI Fans Server”，是一名绝佳的Vtuber的粉丝麦块服务器，建立于2020年7月23日。目前......',
