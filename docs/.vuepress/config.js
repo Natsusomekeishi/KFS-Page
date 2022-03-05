95432d707aa3db6694b5ae07cddc7e2af9b84002
Keishi <natsusome@qq.com>
2022-03-05 14:36:41 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -62,7 +62,23 @@ module.exports = {
         }],
         ['@vuepress/back-to-top'],
         ['autometa', autometa_options],
-        ['@vuepress/last-updated']
+        ['@vuepress/last-updated'],
+        ['@vuepress-reco/vuepress-plugin-bgm-player',{
+            audios: [
+                {
+                name: 'LOSER',
+                artist: '米津玄師',
+                url: 'https://www.ytmp3.cn/down/73654.mp3',
+                cover: 'https://p1.music.126.net/qTSIZ27qiFvRoKj-P30BiA==/109951165895951287.jpg?param=200y200'
+                }
+            ] ,
+            // 是否默认缩小
+            autoShrink: true ,
+            // 缩小时缩为哪种模式
+            shrinkMode: 'float',
+            // 悬浮窗样式
+            floatStyle:{ bottom: '10px', 'z-index': '999999' }
+        }]
     ]
     // plugins: {
     //     'sitemap': {
