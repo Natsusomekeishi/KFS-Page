6638095a01f627124ad378f1a444d49d5b24ecd3
Keishi <natsusome@qq.com>
2022-03-05 15:37:42 +0800

    bgm增加

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -70,6 +70,48 @@ module.exports = {
                 artist: 'cinnamons/evening cinema',
                 url: 'https://www.ytmp3.cn/down/76217.mp3',
                 cover: 'https://www.mckfs.top/favicon.ico'
+                },
+                {
+                name: '群青',
+                artist: 'YOASOBI',
+                url: 'https://www.ytmp3.cn/down/76218.mp3',
+                cover: 'https://www.mckfs.top/favicon.ico'
+                },
+                {
+                name: '花に亡霊',
+                artist: 'ヨルシカ',
+                url: 'https://www.ytmp3.cn/down/76219.mp3',
+                cover: 'https://www.mckfs.top/favicon.ico'
+                },
+                {
+                name: 'カタオモイ',
+                artist: 'Aimer',
+                url: 'https://www.ytmp3.cn/down/76220.mp3',
+                cover: 'https://www.mckfs.top/favicon.ico'
+                },
+                {
+                name: 'ひとり旅',
+                artist: '瀬名航/鎖那',
+                url: 'https://www.ytmp3.cn/down/76221.mp3',
+                cover: 'https://www.mckfs.top/favicon.ico'
+                },
+                {
+                name: 'ツギハギスタッカート',
+                artist: 'nameless',
+                url: 'https://www.ytmp3.cn/down/76222.mp3',
+                cover: 'https://www.mckfs.top/favicon.ico'
+                },
+                {
+                name: '可愛くなりたい',
+                artist: '鎖那',
+                url: 'https://www.ytmp3.cn/down/76223.mp3',
+                cover: 'https://www.mckfs.top/favicon.ico'
+                },
+                {
+                name: '世界は恋に落ちている',
+                artist: 'かぴ',
+                url: 'https://www.ytmp3.cn/down/76224.mp3',
+                cover: 'https://www.mckfs.top/favicon.ico'
                 }
             ] ,
             // 是否默认缩小
